9c08eb8a446c7849f7b94aaf05b979c6ffc63dd2
Daniel Wanja <d@n-so.com>
2012-04-11 21:07:05 -0600

    In progress, updating app and slides.

---
+++ iPadAppsWithFlex/comp.sh
@@ -0,0 +1,12 @@
+mxmlc +configname=airmobile src/app01_ViewNavigatorApp.mxml
+mxmlc +configname=airmobile src/app02_TabbedApp.mxml
+mxmlc +configname=airmobile src/app03_ViewNavigator.mxml
+mxmlc +configname=airmobile src/app04_TabbedViewNavigator.mxml
+mxmlc +configname=airmobile src/app05_SplitViewNavigator.mxml
+mxmlc +configname=airmobile src/app06_SplitViewNavigator.mxml
+mxmlc +configname=airmobile src/app07_OrientationChange.mxml
+mxmlc +configname=airmobile src/app20_Components.mxml
+
+# $ adl  -profile mobileDevice -screensize iPad src/O7_SplitViewNavigator2-app.xml 
+# http://help.adobe.com/en_US/flex/mobileapps/WS19f279b149e7481c-24dc70c812b9cbf7285-8000.html#WS19f279b149e7481c388c771c12b9cd5afde-8000
+# http://help.adobe.com/en_US/air/build/air_buildingapps.pdf
